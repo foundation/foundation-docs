7c0b0f0e94b163ab17606b92d3b60e337f19af79
Geoff Kimball <geoff@zurb.com>
2015-12-14 11:30:25 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  buildSearch: require('./lib/buildSearch'),
+  handlebars: require('./lib/handlebars'),
+  marked: require('./lib/marked'),
+  componentTemplate: 'node_modules/foundation-docs/templates/component.html'
+}
