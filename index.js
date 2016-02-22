40bb1fe717ba8d19469810247a18922ac2f62307
Geoff Kimball <geoff@zurb.com>
2016-02-22 15:21:01 -0800

    Remove build search function, which is now handled by Supercollider

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = {
-  buildSearch: require('./lib/buildSearch'),
   handlebars: require('./lib/handlebars'),
   marked: require('./lib/marked'),
   componentTemplate: 'node_modules/foundation-docs/templates/component.html',
