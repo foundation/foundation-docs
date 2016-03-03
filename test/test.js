5ad1bb8e8cec119eeffd1073559e5696a93b378e
Geoff Kimball <geoff@zurb.com>
2016-03-02 16:03:38 -0800

    Add tests for util libraries, and use mkdirp in buildInkySample to create output directory without errors

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
 var expect = require('chai').expect;
 var fs = require('fs');
-var handlebars = require('handlebars');
-var supercollider = require('supercollider');
-var docs = require('..');
+var rimraf = require('rimraf');
 
 describe('Page Template', function() {
+  var docs = require('..');
+  var handlebars = require('handlebars');
+  var supercollider = require('supercollider');
+
   it('produces a template leveraging Markdown, SassDoc, and JSDoc data', function(done) {
     supercollider
       .config({
@@ -12,7 +14,8 @@ describe('Page Template', function() {
         dest: 'test/output',
         template: 'templates/component.html',
         marked: docs.marked,
-        handlebars: docs.handlebars
+        handlebars: docs.handlebars,
+        quiet: true
       })
       .adapter('sass')
       .adapter('js')
@@ -21,6 +24,64 @@ describe('Page Template', function() {
     stream.on('finish', function() {
       expect(fs.existsSync('test/output/component.html')).to.be.true;
       done();
+    });
+  });
+});
+
+describe('Utilities', function() {
+  describe('buildInkySample', function() {
+    var buildInkySample = require('../lib/util/buildInkySample');
+
+    afterEach(function(done) {
+      rimraf('_build', done);
+    });
+
+    it('creates two HTML code samples with Inky and HTML code', function() {
+      var input = '<container></container>';
+      var output = buildInkySample(input);
+
+      expect(output).to.contain('hljs');
+      expect(output).to.contain('container');
+      expect(output).to.contain('table')
+    });
+
+    it('creates a HTML file with the rendered HTML example', function(done) {
+      var input = '<container></container>';
+      var output = buildInkySample(input);
+
+      fs.exists('_build/examples/example-1.html', function(exists) {
+        expect(exists).to.be.true;
+        done();
+      });
+    });
+  });
+
+  describe('escape', function() {
+    var escape = require('../lib/util/escape');
+
+    it('escapes a string for use in an HTML anchor', function() {
+      var escapedText = escape('Title of Section');
+      expect(escapedText).to.equal('title-of-section');
+    });
+  });
+
+  describe('writeHeading', function() {
+    var writeHeading = require('../lib/util/writeHeading');
+
+    it('writes a section heading with custom classes, an escaped ID, and an anchor icon', function() {
+      var heading = writeHeading('Section Title', 2);
+
+      expect(heading).to.contain('<h2 id="section-title" class="docs-heading"');
+      expect(heading).to.contain('Section Title');
+      expect(heading).to.contain('<a class="docs-heading-icon" href="#section-title"></a>');
+      expect(heading).to.contain('</h2>');
+    });
+
+    it('allows for a custom anchor to be defined', function() {
+      var heading = writeHeading('Section Title', 2, 'custom-anchor');
+
+      expect(heading).to.contain('<h2 id="custom-anchor"');
+      expect(heading).to.contain('href="#custom-anchor"');
     });
   });
 });
