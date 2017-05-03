89aed0ac9ab52559f90c289ebda85ef1c8227cec
Kevin Ball <kmball11@gmail.com>
2017-05-03 11:44:18 -0700

    New scrolalble video

--- js/docs.video.js
+++ js/docs.video.js
@@ -1,7 +1,60 @@
-$('.docs-video-trigger').click(function() {
-  var container = $(this).next('.docs-video-container');
-  container.toggleClass('is-Open');
-  var embed = container.find('.responsive-embed');
-  var iframe = embed.find('iframe');
-  iframe.detach().appendTo(embed);
-});
+// 2. This code loads the IFrame Player API code asynchronously.
+
+if ($('#main-video').is('*')) {
+  var $videoOuter = $('#subpage-intro-video');
+  var $videoInner = $videoOuter.find('.docs-video-inner');
+  var videoId = $('#main-video').data().video;
+  var tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('main-video', {
+      height: '385',
+      width: '690',
+      videoId: videoId,
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+  }
+
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING) {
+      $videoInner.addClass('playing');
+    } else {
+      $videoInner.removeClass('playing');
+    }
+  }
+
+  var $window = $(window);
+  
+  $(window).on("scroll", function() {
+    var videoOffset = $videoOuter.offset().top;
+    var myScrollPosition = $(this).scrollTop();
+ 
+    if (myScrollPosition > videoOffset) {
+      $videoInner.addClass('is-stuck');
+    } else {
+      $videoInner.removeClass('is-stuck');
+    }
+  });
+
+  $('[data-close-video]').on('click', function() {
+    player.stopVideo();
+  });
+
+}
