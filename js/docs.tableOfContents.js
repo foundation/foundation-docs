425ffb2af80e11cb4fbd21f82d90ab4f0970b36d
Kevin Ball <kmball11@gmail.com>
2017-05-04 11:15:15 -0700

    Redo navigation layout

--- js/docs.tableOfContents.js
+++ js/docs.tableOfContents.js
@@ -26,35 +26,35 @@ if ($toc.length) {
 
 }();
 
-var $window = $(window);
-var $container = $('.docs-component');
-var $TOC = $('#docsToc');
-var $TOCparent = $TOC.parent();
-
-if($TOC.is('*')) {
-  var fixedTop = $container.offset().top;
-  var innerHeight = 0;
-  $container.children().each(function() {innerHeight += $(this).height()});
-  var containerHeight = $container.height();
-  var handler = function() {
-    var tocHeight = $TOC.height();
-    var parentOffset = $TOCparent.offset().top;
-    var windowScroll = $window.scrollTop();
-    var containerScroll = $container.scrollTop();
-    var visibleNav = (containerHeight + fixedTop) - windowScroll;
-
-    if (visibleNav < tocHeight || containerScroll + tocHeight >= innerHeight) {
-      $TOC.removeClass('is-fixed');
-      $TOC.addClass('is-docked').css({top: 'auto'});
-    } else if ((windowScroll > fixedTop && parentOffset < windowScroll)|| (parentOffset < fixedTop - windowScroll)) {
-      var offset = Math.max(fixedTop - windowScroll, 0)
-      $TOC.removeClass('is-docked');
-      $TOC.addClass('is-fixed').css({top: offset});
-    } else {
-      $TOC.removeClass('is-fixed');
-      $TOC.removeClass('is-docked');
-    }
-  };
-  $(document).on('load scroll', handler);
-  $container.on('scroll', handler);
-}
+//var $window = $(window);
+//var $container = $('.docs-component');
+//var $TOC = $('#docsToc');
+//var $TOCparent = $TOC.parent();
+//
+//if($TOC.is('*')) {
+//  var fixedTop = $container.offset().top;
+//  var innerHeight = 0;
+//  $container.children().each(function() {innerHeight += $(this).height()});
+//  var containerHeight = $container.height();
+//  var handler = function() {
+//    var tocHeight = $TOC.height();
+//    var parentOffset = $TOCparent.offset().top;
+//    var windowScroll = $window.scrollTop();
+//    var containerScroll = $container.scrollTop();
+//    var visibleNav = (containerHeight + fixedTop) - windowScroll;
+//
+//    if (visibleNav < tocHeight || containerScroll + tocHeight >= innerHeight) {
+//      $TOC.removeClass('is-fixed');
+//      $TOC.addClass('is-docked').css({top: 'auto'});
+//    } else if ((windowScroll > fixedTop && parentOffset < windowScroll)|| (parentOffset < fixedTop - windowScroll)) {
+//      var offset = Math.max(fixedTop - windowScroll, 0)
+//      $TOC.removeClass('is-docked');
+//      $TOC.addClass('is-fixed').css({top: offset});
+//    } else {
+//      $TOC.removeClass('is-fixed');
+//      $TOC.removeClass('is-docked');
+//    }
+//  };
+//  $(document).on('load scroll', handler);
+//  $container.on('scroll', handler);
+//}
