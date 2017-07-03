526cb6c65316c220bbae32b6bf71d6d19a93785c
Kevin Ball <kmball11@gmail.com>
2017-07-03 15:33:44 -0700

    Tweak magellan offset

--- js/docs.tableOfContents.js
+++ js/docs.tableOfContents.js
@@ -20,7 +20,7 @@ $h2s.each(function() {
 // Initialize Magellan on the generated table of contents
 if ($toc.length) {
   if (typeof Foundation !== 'undefined') {
-    new Foundation.Magellan($toc, {barOffset: 80});
+    new Foundation.Magellan($toc, {offset: 130});
   }
 }
 
